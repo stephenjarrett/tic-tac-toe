78fb04d35322c1d6382f5d30225177418fa2d4b9
Stephen Jarrett <sjarrett@insiten.com>
2019-05-14 09:08:33 -0400

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  MatIconModule,
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatAutocompleteModule,
+  MatSidenavModule,
+  MatListModule,
+  MatSelectModule,
+  MatSnackBarModule,
+  MatExpansionModule,
+  MatRadioModule
+} from '@angular/material';
+
+const Material = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatAutocompleteModule,
+  MatSelectModule,
+  MatTabsModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatIconModule,
+  MatSortModule,
+  MatSnackBarModule,
+  MatExpansionModule,
+  MatRadioModule
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    Material,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
